cf258efad28098fef03f4cd8b514d8122e118073
Nyx0uf <benjgodard@me.com>
2013-06-26 13:50:38 +0200

    Update copyright
    What a hassle

--- src/MMGAPNSConnection.cpp
+++ src/MMGAPNSConnection.cpp
@@ -1,7 +1,7 @@
 //
 // MMGAPNSConnection.cpp
 //
-// Copyright (c) 2013 @Nyx0uf. All rights reserved.
+// Copyright (c) 2013 MacGeneration. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
--- src/global.hpp
+++ src/global.hpp
@@ -1,7 +1,7 @@
 //
 // global.hpp
 //
-// Copyright (c) 2013 @Nyx0uf. All rights reserved.
+// Copyright (c) 2013 MacGeneration. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
